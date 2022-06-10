f63c9f05e7337826c2c46970fc96e2e415d4c1f2
gilvat <gilvat13@gmail.com>
2022-06-10 20:31:12 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Anton Aksenov");
+        System.out.println("23.07.1984");
+    }
+}
