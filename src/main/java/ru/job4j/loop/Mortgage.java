1dfb7aba219ec40cdb1d3fd524ded49b3f8e13a8
gilvat <gilvat13@gmail.com>
2022-06-14 19:41:40 +0300

    5.7. Ипотека [#156315 #312096]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,8 +2,8 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        while (amount * (1 + percent / 100) - salary > 0) {
+        int year = 0;
+        while (amount  > 0) {
             amount = amount * (1 + percent / 100) - salary;
             year++;
         }
